97d0ddda8fb7c739876841499da0b3a6988d6f80
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-06-07 16:33:50 -0500

    Now it's possible to load the data from the file

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,20 +6,40 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class Main extends Application{
+
+	private static final String SAVE_PATH = "save-file.dm";
 	
 	private MainWindowsController mwc;
 	private AcademyScheduleUsersManager as;
 	
 	public Main() {
-		as = new AcademyScheduleUsersManager();
+		try {
+			loadAllData();
+		} catch(IOException | ClassNotFoundException e) {
+			as = new AcademyScheduleUsersManager();
+		}
 		mwc = new MainWindowsController(as);
 	}
 	
 	public static void main(String[] args){
 		launch(args);
 	}//End main
+
+	public void loadAllData() throws IOException, ClassNotFoundException {
+		File f = new File(SAVE_PATH);
+		if(!f.exists()) {
+			f.createNewFile();
+		}
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+		as = (AcademyScheduleUsersManager) ois.readObject();
+		ois.close();
+	}//End loadAllData
 
 	@Override
 	public void start(Stage window) throws Exception {
